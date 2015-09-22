e9cc172323eae4f0fab9a456856675cf2e1c7a5f
jenseschouten <jense.schouten@hotmail.com>
2015-09-22 10:46:43 +0200

    sdfgh

--- CIMS/src/cims/Field_Operations/Task.java
+++ CIMS/src/cims/Field_Operations/Task.java
@@ -5,6 +5,9 @@
  */
 package cims.Field_Operations;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author Jense
@@ -15,14 +18,89 @@ public class Task {
     private String name;
     private String urgency;
     private String status;
-    
+    private String description;
+    private String location;
+    private ArrayList<Unit> units;
 
-    public void task(int taskID) {
+    public void task(int taskID, String name, String urgency, String status, String location, String description) {
         this.taskID = taskID;
+        this.name = name;
+        this.urgency = urgency;
+        this.status = status;
+        this.location = location;
+        this.description = description;
     }
 
     public int getTaskID() {
         return this.taskID;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        if (!this.name.equals(name)) {
+            this.name = name;
+        }
+    }
+
+    public String getUrgency() {
+        return this.urgency;
+    }
+
+    public void setUrgency(String urgency) {
+        if (!this.urgency.equals(urgency)) {
+            this.urgency = urgency;
+        }
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setstatus(String status) {
+        if (!this.status.equals(status)) {
+            this.status = status;
+        }
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        if (!this.location.equals(location)) {
+            this.location = location;
+        }
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setdescription(String description) {
+        if (!this.description.equals(description)) {
+            this.description = description;
+        }
+    }
+
+    public ArrayList<Unit> getUnits() {
+        return this.units;
+    }
+
+    public Unit getUnit(int ID) {
+        for (Unit unit : units) {
+            if (unit.getUnitID() == ID) {
+                return unit;
+            }
+        }
+        return null;
+    }
+
+    public void addUnit(Unit unit) {
+        if (!this.units.contains(unit)) {
+            this.units.add(unit);
+        }
+    }
 }
