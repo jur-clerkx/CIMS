99749512e22bf68e493a8ea1e07dd54d307f2028
Bazzzzz <sebastiaan.koch@hotmail.com>
2015-10-06 14:00:20 +0200

    CreateUnitButton start

--- CIMS/src/cims/CreateTaskController.java
+++ CIMS/src/cims/CreateTaskController.java
@@ -5,6 +5,7 @@
  */
 package cims;
 
+import cims.Field_Operations.Unit;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -93,6 +94,15 @@ public class CreateTaskController implements Initializable {
     }
      @FXML
     private void createButtonClick(MouseEvent event) {
+        int taskID = Integer.parseInt(textfieldTaskID.getText());
+        String taskName = textfieldTaskName.getText();
+        String taskLocation = textfieldTaskLocation.getText();
+        String urgency = comboboxUrgency.getSelectionModel().getSelectedItem().toString();
+        String status = comboboxStatus.getSelectionModel().getSelectedItem().toString();
+        
+        
+        
+        OperatorMainController.myController.createTask();
     }
      @FXML
     private void cancelButtonClick(MouseEvent event) {
