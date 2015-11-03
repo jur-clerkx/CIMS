933912472797bdf277de414b9bf8a98e15f1e7a1
Nickvdmullen <Nickvdmullen@gmail.com>
2015-11-03 10:24:39 +0100

    SA Project

---
+++ CIMS_SA/src/Connection/ConnectionController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connection;
+
+import Situational_Awareness.User;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ *
+ * @author Nick van der Mullen
+ */
+public class ConnectionController {
+    
+    private static String serverAddress;
+    public static User user;
+    private static Socket s;
+    static ObjectOutputStream output;
+    static ObjectInputStream input;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
