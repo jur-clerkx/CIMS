706e98a96de800f7cf09e58e9f5b278ff2c84487
RickvanPoppel <rickvanpoppel@hotmail.com>
2016-01-12 11:03:27 +0100

    twitter commit

--- CIMS_SA/src/Situational_Awareness/Information.java
+++ CIMS_SA/src/Situational_Awareness/Information.java
@@ -8,6 +8,7 @@ package Situational_Awareness;
 import Field_Operations.Task;
 import Network.PublicUser;
 import java.io.Serializable;
+import static java.lang.Math.toIntExact;
 
 /**
  *
@@ -15,7 +16,7 @@ import java.io.Serializable;
  */
 public class Information implements Serializable{
 
-    private int ID;
+    private Long ID;
     private Task task;
     private String name;
     private String description;
@@ -28,7 +29,7 @@ public class Information implements Serializable{
     private boolean Private;
     private Network.PublicUser user;
 
-    public Information(int ID, Task task, String name, String description, String location, int casualities, int toxic, int danger, int impact, String image, Network.PublicUser user, boolean Private) {
+    public Information(Long ID, Task task, String name, String description, String location, int casualities, int toxic, int danger, int impact, String image, Network.PublicUser user, boolean Private) {
         if (ID > 0 && description != null && description.length() < 255 && location != null && location.length() < 120 & casualities >= 0 && toxic >= 0
                 && danger >= 0  && impact > 0 && impact < 10000) {
             this.ID = ID;
@@ -48,6 +49,21 @@ public class Information implements Serializable{
         }
 
     }
+    public Information(Long ID, String name, String description, String location, String image){
+        this.ID = ID;
+        this.task = null;
+        this.name = name; 
+        this.description = description;
+        this.location = location;
+        this.image = image;
+        this.casualties = 0;
+        this.toxic = 0;
+        this.danger = 0;
+        this.impact = 0;
+        this.user = null;
+        this.Private = false;
+        
+    }
 
     public String getName() {
         return name;
@@ -58,7 +74,7 @@ public class Information implements Serializable{
     }
 
     public int getID() {
-        return ID;
+        return toIntExact(ID);
     }
 
     public Task getTaskID() {
